5efb9d8e686ed858b1b1a9cdb67e45a4434a4633
Evgeniy Vafiev <vafiev@poisondrop.ru>
2023-08-22 20:28:31 +0600

    master - add IDE preferences information

--- tests/Http/Controllers/User/LoginFormControllerTest.php
+++ tests/Http/Controllers/User/LoginFormControllerTest.php
@@ -2,10 +2,24 @@
 
 namespace Tests\Http\Controllers\User;
 
-use App\Http\Controllers\User\LoginFormController;
-use PHPUnit\Framework\TestCase;
+use App\Model\User;
+use Tests\TestCase;
 
 class LoginFormControllerTest extends TestCase
 {
+    public function testGuest()
+    {
+        $response = $this
+            ->get(route('login.show'));
+        $response->assertOk();
+        $response->assertStatus(200);
+    }
 
+    public function testUser()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->get(route('login.show'));
+        $response->assertRedirect();
+    }
 }
